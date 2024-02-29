3b58dc5af52d7907c33ad8d1539edaa891395fe5
Macsik121 <hiomimipad@gmail.com>
2024-02-29 21:59:51 +0300

    The most basic server which only serves static files. Just webpack setup for bundling react project which now has The only and the root component "App"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return (
+    <div>App.jsx!</div>
+  );
+}
+
+ReactDOM.createRoot(
+  document.getElementById('root'),
+).render(<App />);
